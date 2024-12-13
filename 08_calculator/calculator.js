3a1a3e76dc537fd7422ac82363f2d1b79ad6e145
WillsMcGraw <wills.mcgraw21@gmail.com>
2024-12-13 14:44:34 -0500

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => {
+    return total *= num;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let arr = []
+  for (let i = 1; i <= a; i++) {
+    arr.push(i);
+  }
+  if (arr.length == 0) {
+    return 1;
+  }
+  return arr.reduce((total, num) => {
+    return total *= num;
+  }, 1);
 };
 
 // Do not edit below this line
